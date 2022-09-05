9c82057217833527f781e5d68b4d3c7dabd45ca6
LesterCerioli <lesterlucasit@hotmail.com>
2022-09-05 19:03:40 -0300

    "fet(#8): Building Web classes and contracts"

---
+++ source/Infrastructure/WebStore.Infrastructure/Web/SmartTable/Search.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.Web.SmartTable
+{
+    public class Search
+    {
+        public JObject PredicateObject { get; set; }
+    }
+}
