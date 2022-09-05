3ec2ff2386b2521a376c6221e987a97c84780793
LesterCerioli <lesterlucasit@hotmail.com>
2022-09-05 15:24:33 -0300

    "fet(#3): implmenting Unit test into Infrastructure Project"

---
+++ test/WebStore.Infrastructure.Tests/StringHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Helpers;
+using Xunit;
+
+namespace WebStore.Infrastructure.Tests
+{
+    public class StringHelperTests
+    {
+        [Fact]
+        public void UrlWithOneSpaceShouldBeReplacedWithOneDash()
+        {
+            var foo = "friendly url".ToUrlFriendly();
+            Assert.Equal("friendly-url", foo);
+        }
+
+        [Fact]
+        public void UrlWithManySpacesShouldBeReplacedWithOneDash()
+        {
+            var foo = "friendly    url".ToUrlFriendly();
+            Assert.Equal("friendly-url", foo);
+        }
+
+        [Fact]
+        public void UrlWithTwoDashesShouldBeReplacedWithOneDash()
+        {
+            var foo = "friendly--url".ToUrlFriendly();
+            Assert.Equal("friendly-url", foo);
+        }
+
+        [Fact]
+        public void UrlWithManyDashesShouldBeReplacedWithOneDash()
+        {
+            var foo = "friendly---url".ToUrlFriendly();
+            Assert.Equal("friendly-url", foo);
+        }
+    }
+}
