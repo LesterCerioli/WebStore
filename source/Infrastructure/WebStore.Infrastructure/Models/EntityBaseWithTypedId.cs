32f1cdc0688b2139a425c587378da1903e8bca9a
LesterCerioli <lesterlucasit@hotmail.com>
2022-09-05 12:27:25 -0300

    Initial repository

---
+++ source/Infrastructure/WebStore.Infrastructure/Models/EntityBaseWithTypedId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.Models
+{
+
+    
+    public abstract class EntityBaseWithTypedId<TId> : ValidatableObject, IEntityWithTypedId<TId>
+    {
+        public virtual TId Id { get; protected set; }
+    }
+}
---
+++ test/WebStore.Modules.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace WebStore.Modules.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
