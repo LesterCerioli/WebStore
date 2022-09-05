8a3037a88f825b80aef3d85cab030655dd6da212
LesterCerioli <lesterlucasit@hotmail.com>
2022-09-05 17:00:14 -0300

    fet(#6): Building Module classes and Contracts

---
+++ source/Infrastructure/WebStore.Infrastructure/Modules/IModuleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.Modules
+{
+    public interface IModuleInitializer
+    {
+        void ConfigureServices(IServiceCollection serviceCollection);
+
+        //void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+    }
+}
---
+++ source/Presentation/WebStore.WebHosting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebStore.WebHosting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
