1597992f77f4001e1556da905f4200efd5c3001c
LesterCerioli <lesterlucasit@hotmail.com>
2022-09-06 04:43:06 -0300

    "fet(#16): Building Core Entity models"

---
+++ source/Modules/WebStore.Modules.Core/Models/WebStoreConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Modules.Core.Models
+{
+    public class WebStoreConstants
+    {
+        public const string ThemeConfigKey = "Theme";
+    }
+}
